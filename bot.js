0747b720e174b5d10e0302fbd2703f2b5320f151
Jelmer405 <67926718+Jelmer405@users.noreply.github.com>
2020-07-07 13:38:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,6 +10,7 @@ client.on('ready', () => {
 
 });
 
+ 
 
 client.on('message', message => {
 
@@ -18,28 +19,23 @@ client.on('message', message => {
        message.reply('Vis Wax combinatie: https://secure.runescape.com/m=forum/sl=0/forums?75,76,331,66006366');
 
        }
+ else if (message.content === '!gainz') {
+      message.reply('Zoek het lekker zelf op: https://www.runeclan.com');
+  
+ }
+ 
+ else if (message.content === '!alog') {
+     message.reply('Ook de alog mag je nog zelf opzoeken: https://apps.runescape.com/runemetrics/app/welcome');
+
+ } 
+ 
+ else if (message.content === '!merchant') { 
+  message.reply('De merchant heeft vandaag: https://runescape.wiki/w/Travelling_Merchant%27s_Shop'); 
+ }
 });
 
-
-client.on('message', message => {
-
-    if (message.content === '!gainz') {
-
-       message.reply('Zoek het lekker zelf op: https://www.runeclan.com);
-
-       }
-    else if (message.content === '!merchant') {
-       message.reply('De merchant heeft vandaag: https://runescape.wiki/w/Travelling_Merchant%27s_Shop');
-    }
-   else if (message.content === '!alog') {
-       message.reply('Ook de alog mag je nog zelf opzoeken: https://apps.runescape.com/runemetrics/app/welcome');
-    }
-    
-});
-
-
+ 
 
 // THIS  MUST  BE  THIS  WAY
-
 
 client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
