5529111864761353d411923bd3b80fdd86cda07d
Jelmer405 <67926718+Jelmer405@users.noreply.github.com>
2020-07-07 09:53:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,19 +33,9 @@ client.on('message', message => {
        message.reply('De merchant heeft vandaag: https://runescape.wiki/w/Travelling_Merchant%27s_Shop');
     }
 });
- 
-client.on('message', message => {
 
-    if (message.content === '!stats '+playername) {
-
-       message.reply('Hier zijn jouw gainz van vandaag: https://www.runeclan.com/user/'+playername);
-
-       }
-});
 
 // THIS  MUST  BE  THIS  WAY
-
-
 
 
 client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
