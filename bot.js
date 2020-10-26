e13d2da5392cd3764d5db57ab33e45c3b8059346
Jelmer405 <67926718+Jelmer405@users.noreply.github.com>
2020-10-26 07:57:27 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,13 +20,13 @@ client.on('message', message => {
 
        }
  
- /   if (message.content === '!vis') {
+    if (message.content === '!visdffdsdfsdfsds') {
 
        message.reply('Vis Wax combinatie: https://secure.runescape.com/m=forum/sl=0/forums?75,76,331,66006366');
 
        }
   
-/  if (message.content === '%vis') {
+    if (message.content === '%vifdfsfdsdffdsfdss') {
 
        message.reply('Vis Wax combinatie: https://tinyurl.com/viswaxcombination');
 
